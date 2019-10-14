7ac299cda8183a430c2e2b0d9f01d3d8e474f1c3
AlejandroHerr <ahernandezc@protonmail.com>
2019-10-14 22:20:45 +0200

    feat(propertycontroller): add NodeController constructor and #create method

--- src/homie/application/PropertyController.ts
+++ src/homie/application/PropertyController.ts
@@ -1,1 +1,25 @@
-export default class PropertyController {}
+import Property from '../domain/Property';
+import HomiePublisher from '../services/HomiePublisher';
+
+export default class PropertyController {
+  readonly property: Property;
+
+  readonly homiePublisher: HomiePublisher;
+
+  constructor({ property, homiePublisher }: { property: Property; homiePublisher: HomiePublisher }) {
+    this.property = property;
+    this.homiePublisher = homiePublisher;
+  }
+
+  static create({
+    property,
+    homiePublisher,
+  }: {
+    property: Property;
+    homiePublisher: HomiePublisher;
+  }): PropertyController {
+    const propertyController = new PropertyController({ property, homiePublisher });
+
+    return propertyController;
+  }
+}
